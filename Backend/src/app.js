12a2d8be85fd0fb99198c1ffa615a4e194ed9c64
Nanda Bendkoli <bendkolinanda12@gmail.com>
2026-06-04 10:21:53 +0530

    added frontend url cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
+            "https://shop-hub-ecommerce-website-nucj.vercel.app/"
         ],
         credentials: true,
     })
